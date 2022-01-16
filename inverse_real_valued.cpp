36a815d1973c2780daa3fc6ade28c22f2ddd78e8
Evan Lauer <lauerevan@gmail.com>
2022-01-16 17:23:15 -0600

    Added new headers to inverse_real_valued.cpp.

--- inverse_real_valued.cpp
+++ inverse_real_valued.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -21,6 +22,40 @@ class Matrix
 };
 
 /**
+ * @brief Calculates determinant of matrix m.
+ * 
+ * @param m Matrix*
+ * @return double 
+ */
+double matrix_determinant(Matrix* m)
+{
+
+}
+
+/**
+ * @brief Transposes matrix m
+ * 
+ * @param m Matrix*
+ */
+void transpose_matrix(Matrix* m)
+{
+
+}
+
+/**
+ * @brief Calculates the inverse of matrix m. Returns as new matrix.
+ * 
+ * @param m Matrix*
+ * @return Matrix* 
+ */
+Matrix* matrix_inverse(Matrix* m)
+{
+
+}
+
+
+
+/**
  * @brief Handles index arithmetic for 2d->1d array.
  * 
  * @param size int 
@@ -29,22 +64,6 @@ class Matrix
  * @return int     1d index
  */
 int calculate_index(int size, int row, int col) { return (row * size) + col; }
-
-
-/**
- * @brief Sets the value at (row, col) to val.
- * 
- * @param m Matrix*
- * @param row int
- * @param col int
- * @param val double
- */
-/*
-void matrix_set(Matrix* m, int row, int col, double val)
-{
-}
-*/
-
 
 /**
  * @brief Gets the value at (row, col).
@@ -67,12 +86,24 @@ Matrix* populate_matrix()
     cout<< "Square matrix dimension (should be integer > 2): ";
     int dimension;
     cin>> dimension;
+    Matrix* m = new Matrix(dimension);
     if (dimension < 3) { cerr<< "Dimension should be integer > 2."; exit(-1); }
-    
-
+    for (int i = 0; i < dimension; i++)
+    {
+        cout<<"\nRow " + to_string(i+1) + ":\n";
+        for (int j = 0; j < dimension; j++)
+        {
+            cout<<"Entry A " + to_string(i + 1) + to_string(j + 1) + ": ";
+            double entry;
+            cin>>entry;
+            m->matrix.push_back(entry);
+        }
+    }
+    return m;
 }
 
 int main()
 {
+    Matrix* m = populate_matrix();
     return 0;
 };
